fcc72688d948b8d2465d8e106fb98e41c718ae4a
Alex Bonet Canela <bonet.canela.alex@gmail.com>
2024-09-25 19:29:48 +0200

    Deleted comments on Nivell 1 Exercici 1

--- Nivell1/Exercici1/src/main/java/org/alexandra/Main.java
+++ Nivell1/Exercici1/src/main/java/org/alexandra/Main.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 public class Main {
     public static void main(String[] args) {
 
-        //1.Add All months of the year except August
         ArrayList<Month> year = new ArrayList<>();
         year.add(new Month("January"));
         year.add(new Month("February"));
@@ -23,15 +22,14 @@ public class Main {
 
         printMonths(year, "Inserting all Months except August:");
 
-        //2.Add August
         year.add(7, new Month("August"));
         printMonths(year, "Adding August:");
 
-        //3. Convert ArrayList in HashSet:
+
         HashSet<Month> yearSet = new HashSet<>(year);
         printMonths(yearSet, "Inserting All months into a HashSet: ");
 
-        //4. Add duplicate August value to HashSet:
+
         yearSet.add(new Month("August"));
         printMonths(yearSet, "Duplicated August silently ignored: ");
 
